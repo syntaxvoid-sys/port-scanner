51bb65abf3b2987919f483aa90014a544d4b1251
syntaxvoid-sys <leonfink333@gmail.com>
2026-08-30 00:20:05 +0200

    add port scanner

---
+++ port_scanner.py
@@ -0,0 +1,10 @@
+import socket
+target_ip = input("Enter targets ip here:")
+
+for port in range(80, 444): #change the port range here i dont know how to do it in a input yet. Remember at the end 444 is 443 and for example 334 is 333
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try: 
+        s.connect((target_ip, port))
+        print(f"Port {port} is open")
+    except:
+        print(f"Port {port} is failed")
