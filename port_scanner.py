d67802303b59b5292f8aa7cc49b031a142b893b7
syntaxvoid-sys <leonfink333@gmail.com>
2026-09-01 23:24:43 +0200

    Modify port range input for port scanner
    Updated port range to accept user input for start and end values.

--- port_scanner.py
+++ port_scanner.py
@@ -1,7 +1,7 @@
 import socket
 target_ip = input("Enter targets ip here:")
 
-for port in range(80, 444): #change the port range here i dont know how to do it in a input yet. Remember at the end 444 is 443 and for example 334 is 333
+for port in range(input("enter port number start: "), input("enter port number end: ")):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try: 
         s.connect((target_ip, port))
